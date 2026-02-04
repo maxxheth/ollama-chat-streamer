754ab3326154f19f1f36289219db7d88aeac1e35
Maximillian Heth <info@edfx.co>
2026-02-04 06:43:06 -0600

    fix: remove session logging from choose.sh script

--- choose.sh
+++ choose.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-docker compose run --rm chat --dest sessions/session-$(datestr).log --context . --context-grep 'md,py,Dockerfile,log,yml' --persist-to-db --select-session
+docker compose run --rm chat --context . --context-grep 'md,py,Dockerfile,log,yml' --persist-to-db --select-session
